2bbcc477a7a69e9b3c3d8082ebf8bcf63bcab039
garethfoote <gareth.foote@gmail.com>
2013-01-22 14:56:52 +0000

    Added gfte module containing events class. Finished traversal class, which is now using a file type white list

--- collector/main.py
+++ collector/main.py
@@ -1,5 +1,28 @@
+import logging, re
 from postagging import PartOfSpeechTagger
+from traverse import Traverse
 
+# Logging
+with open('debug.log', 'w'):
+        pass
+logging.basicConfig(filename='debug.log',level=logging.DEBUG)
+
+# Directory traversal
+def fileFoundHandler(filePath):
+    print filePath
+    f = open(filePath, 'r')
+    content = f.read()
+
+traverse = Traverse(['traverse-test'])
+traverse.restrict({'maxSize': 20*1024*1024 })
+# traverse.allowTypes(['text/plain', 'text/html', 'application/pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'])
+traverse.allowTypes(['application/msword'])
+traverse.onFilePass += fileFoundHandler
+traverse.start()
+
+
+"""
+# Part of Speech Tagging
 pos_tagger = PartOfSpeechTagger(['RB', 'JJ', 'JJR', 'AJS', 'NN', 'NNS', 'NN1', 'PN', 'VB', 'VBP'])
 
 # Get list of all tag shortcode and their defintion+examples
@@ -7,7 +30,7 @@ pos_tagger = PartOfSpeechTagger(['RB', 'JJ', 'JJR', 'AJS', 'NN', 'NNS', 'NN1', '
 # print "%s\nlength:%s" % (''.join(alltags), str(len(alltags)))
 
 # Pass sentence to PoS Tagger and categorise by class
-#print pos_tagger.tag_sentence('And now for something completely different')
+# print pos_tagger.tag_sentence('And now for something completely different')
 pos_tagger.tag_sentence('They refuse to permit us to obtain the refuse permit. The rain in spain falls mainly on the plain.')
 print pos_tagger.get_formatted("<replace class='%s'>%s</replace>")
 # print pos_tagger.get_by_class()
@@ -21,3 +44,4 @@ print pos_tagger.get_formatted("<replace class='%s'>%s</replace>")
 # NN1 - singular noun
 # PN = pronoun
 # VB - verb, base form
+"""
--- collector/traverse.py
+++ collector/traverse.py
@@ -1,26 +1,58 @@
 """
     Dependencies:
-
 """
-
-class Mapping:
-    def __init__(self):
-
-import os
+import os, logging
 from os.path import join, getsize
+from mimetypes import guess_type
 
-maxsizemb = 10
-maxsizebytes = maxsizemb*1024*1024
-permittedtypes = ['txt']
+from gfte.events.eventhook import *
 
-# Traverse from specified start point
-for root, dirs, files in os.walk('collector'):
-    print '\n', root, dirs,
-    for name in files:
-        print name + ' ',
+class Traverse:
 
-    #print root, "consumes",
-    #print sum(getsize(join(root, name)) for name in files),
-    #print "bytes in", len(files), "non-directory files"
-    #if 'ebooks' in dirs:
-    #    dirs.remove('ebooks')  # don't visit CVS directories
+    def __init__(self, root_dirs=['traverse-test']):
+        self.__roots = root_dirs
+        self.__config = { 'maxSize': 0 }
+        self.__permittedTypes = []
+        self.onFilePass = EventHook()
+
+    def start(self):
+        """Start traversal"""
+        # Traverse from specified start point
+        for dir in self.__roots:
+            for root, dirs, files in os.walk(dir):
+                # print '\n', root, dirs,
+                for name in files:
+                    fullPath = join(root, name)
+                    if self.__fileSuccess(fullPath) == True:
+                        self.onFilePass.fire(fullPath)
+
+    def allowTypes(self, permittedTypes):
+        self.__permittedTypes = permittedTypes
+
+    def restrict(self, config):
+        self.__config.update(config)
+
+    def __fileSuccess(self, file):
+        passed = False
+        type = guess_type(file)
+        # Test against file type whitelist.
+        if len(self.__permittedTypes) > 0 :
+            passed = type[0] in self.__permittedTypes
+        else:
+            passed = True
+
+        if passed == False:
+            logging.info('[%s] Type: %s not permitted.' % (file, type[0]))
+
+        # If passed successfully, check against maxSize restrictions
+        if passed == True and self.__config['maxSize'] > 0:
+            passed = getsize(file) < self.__config['maxSize']
+
+        return passed
+
+
+#print root, "consumes",
+#print sum(getsize(join(root, name)) for name in files),
+#print "bytes in", len(files), "non-directory files"
+#if 'ebooks' in dirs:
+#    dirs.remove('ebooks')  # don't visit CVS directories
