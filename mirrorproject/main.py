3c55d63219a0bae8c6f73be8e84df69e3708a6e2
garethfoote <gareth.foote@gmail.com>
2013-03-15 22:16:52 +0000

    Reverted main.py to collector.

--- mirrorproject/main.py
+++ mirrorproject/main.py
@@ -13,23 +13,23 @@ logging.basicConfig(filename='debug.log',level=logging.DEBUG)
 # Configuration.
 cfg = Config('../config.cfg')
 
-"""
+#"""
 # Start collector.
 collector = Collector(cfg.collector, cfg.app.wordClassTags)
 collector.start()
-"""
+#"""
 
 """
 poetic = Poetic(cfg.poetic, cfg.app.wordClassTags)
 poetic.start()
 """
 
-#"""
+"""
 # List tag classes.
 posTagger = PartOfSpeechTagger(cfg.app.wordClassTags)
 
 tags = posTagger.list_tags()
 for item in tags:
     print item
-#"""
 print "Total: "+ str(len(tags))
+"""
