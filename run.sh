23ba3a5c90bc72f0444e72acfef0472a59c2af55
garethfoote <gareth.foote@gmail.com>
2015-09-23 18:13:19 +0100

    Tidy up

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ echo "Config for the env: $INITIAL_ENV" >&2
 # Activate env.
 source $INITIAL_ENV/bin/activate
 # Export nltk data variable for use of lib.
-export NLTK_DATA=${PWD}/nltk_data 
+export NLTK_DATA=${PWD}/nltk_data
 # Change to correct directory and run app.
 cd src
 python main.py --type poemparse
